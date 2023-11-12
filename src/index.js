b352ce614bbf53c1c241b0bbfbb0421af2faccae
LouBell14 <87602844+LouBell14@users.noreply.github.com>
2023-11-12 16:00:01 +0000

    changed description with innerHTML

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  console.log(response.data.condition.decription);
+  let descriptionElement = document.querySelector("#description");
   cityElement.innerHTML = response.data.city;
-
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
